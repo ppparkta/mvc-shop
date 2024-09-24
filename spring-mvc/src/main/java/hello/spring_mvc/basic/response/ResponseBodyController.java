b4d1b5645ca0e1f7aa50dc4131fdc7f3123830d2
ppparkta <pppppark2@gmail.com>
2024-09-25 01:30:37 +0900

    feat: http 메시지에 응답

---
+++ spring-mvc/src/main/java/hello/spring_mvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,58 @@
+package hello.spring_mvc.basic.response;
+
+import hello.spring_mvc.basic.HelloData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+//@Controller
+//@ResponseBody
+@RestController
+public class ResponseBodyController {
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2() throws IOException {
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3() {
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);
+    }
+
+    // 애노테이션을 사용하면 응답코드도 설정할 수 있음 (단순한것만 가능, 동적으로 수정 불가능)
+    // 동적으로 바꿔야 할 때는 ResponseEntity 사용하기
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2() {
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return helloData;
+    }
+}
