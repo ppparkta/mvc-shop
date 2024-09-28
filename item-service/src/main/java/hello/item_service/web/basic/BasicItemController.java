737a1c2697585357067c8abcb21189a259cc170f
ppparkta <pppppark2@gmail.com>
2024-09-28 22:31:31 +0900

    feat: 타임리프 문법 적용

---
+++ item-service/src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package hello.item_service.web.basic;
+
+import hello.item_service.domain.item.Item;
+import hello.item_service.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor // final 키워드가 붙은 필드의 생성자를 만들어줌
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * dummy data
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
