83af72e633b8cbed1ed0add72a47c1093d33a42a
ppparkta <pppppark2@gmail.com>
2024-09-28 23:04:55 +0900

    feat: 상품 등록 처리 1

--- item-service/src/main/java/hello/item_service/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/item_service/web/basic/BasicItemController.java
@@ -4,13 +4,13 @@ import hello.item_service.domain.item.Item;
 import hello.item_service.domain.item.ItemRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -39,9 +39,21 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    // method 이용해서 기능 분리
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String save(@RequestParam("itemName") String itemName,
+                       @RequestParam("price") int price,
+                       @RequestParam("quantity") Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
     }
 
     /**
